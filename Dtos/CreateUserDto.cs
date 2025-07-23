a37965b41436abb061eece75754ddba1ea842be4
Aakash <aakash.kumar@capsitech.com>
2025-07-23 16:07:59 +0530

    cleanup

--- Dtos/CreateUserDto.cs
+++ Dtos/CreateUserDto.cs
@@ -1,34 +1,3 @@
-// namespace MyMongoApp.Dtos
-// {
-//     public class CreateUserDto
-// {
-//     public string Name { get; set; } = string.Empty;
-//     public string Email { get; set; } = string.Empty;
-//     public string Role { get; set; } = string.Empty;
-//     public List<string> ConfigRoles { get; set; } = new();
-// }
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 using MyMongoApp.Enums;
 
 namespace MyMongoApp.Dtos
--- Models/AuditLog.cs
+++ Models/AuditLog.cs
@@ -1,79 +1,3 @@
-//using MongoDB.Bson;
-//using MongoDB.Bson.Serialization.Attributes;
-//using MyMongoApp.Enums;
-
-//public class AuditLog
-//{
-//    [BsonId]
-//    [BsonRepresentation(BsonType.ObjectId)]
-//    public string? Id { get; set; }
-//    public AuditLogEntity EntityType { get; set; } = AuditLogEntity.Unknown;
-//    public string EntityId { get; set; } = string.Empty;  // the LoginRule ID
-//    public string Action { get; set; } = string.Empty;    // Created, Updated, Deleted
-//    public string PerformedBy { get; set; } = string.Empty; // Optional: User who did it
-//    public string Description { get; set; } = string.Empty; // What changed
-//    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
-//}
-
-
-
-
-
-
-//// using MongoDB.Bson;
-//// using MongoDB.Bson.Serialization.Attributes;
-//// using MyMongoApp.Enums;
-
-//// public class AuditLog
-//// {
-////     public string Id { get; set; }
-////     public AuditLogEntity EntityType { get; set; } = AuditLogEntity.Unknown;
-////      public IdNameModel? Target{} // the LoginRule ID
-////     public string Action { get; set; } = string.Empty;    // Created, Updated, Deleted
-////     public string PerformedBy { get; set; } = string.Empty; // Optional: User who did it
-////     public string Description { get; set; } = string.Empty; // What changed
-////     public CreatedBy CreatedBy { get; set; }
-//// }
-
-//// public class IdNameModel
-//// {
-////     [BsonId]
-////     [BsonRepresentation(Object.ObjectId)]
-////     public string Id { get; set; }
-////     public string Name { get; set; } = string.Empty;
-//// }
-
-//// public class CreatedBy
-//// {
-////     [BsonId]
-////     [BsonRepresentation(Object.ObjectId)]
-////     public string Id { get; set; }
-////     /// <summary>
-////     /// 
-////     /// </summary>
-////     public string Name { get; set; } = string.Empty; //User name
-////     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
-//// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using MyMongoApp.Enums;
@@ -89,13 +13,13 @@ namespace MyMongoApp.Models
         public AuditLogEntity EntityType { get; set; } = AuditLogEntity.Unknown;
 
         [BsonRepresentation(BsonType.ObjectId)]
-        public string EntityId { get; set; } = string.Empty; // e.g., LoginRule ID
+        public string EntityId { get; set; } = string.Empty;
 
-        public IdNameModel? Target { get; set; } // optional enriched info
+        public IdNameModel? Target { get; set; }
 
-        public string Action { get; set; } = string.Empty; // e.g., Created, Updated, Deleted
+        public string Action { get; set; } = string.Empty;
 
-        public CreatedBy? PerformedBy { get; set; } // optional user who did it
+        public CreatedBy? PerformedBy { get; set; }
 
         public string Description { get; set; } = string.Empty;
 
